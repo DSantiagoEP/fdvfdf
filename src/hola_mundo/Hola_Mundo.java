b79e3c6b2497dad1fea5b33c6f2ebf88e678bd74
DSantiagoEP <80977293+DSantiagoEP@users.noreply.github.com>
2022-04-04 12:23:05 -0500

    Add files via upload

---
+++ src/hola_mundo/Hola_Mundo.java
@@ -0,0 +1,11 @@
+package hola_mundo;
+
+public class Hola_Mundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+}
